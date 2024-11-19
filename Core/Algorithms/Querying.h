880b1672df79253c4706f1177d580f239321ae55
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2024-11-19 02:50:26 +0100

    Querying improvements

--- Core/Algorithms/Querying.h
+++ Core/Algorithms/Querying.h
@@ -2,9 +2,8 @@
 
 #pragma once
 
-class Querying
+namespace Querying
 {
-public:
     /// <summary>
     /// Counts the number of elements in the enumerator.
     /// </summary>
@@ -20,6 +19,10 @@ public:
     /// <summary>
     /// Sums all elements in the enumerator.
     /// </summary>
+    /// <remarks>
+    /// If the enumerator is empty, the result is the default value of the element type.
+    /// Check for emptiness before calling this method if the default value is not the desired result.
+    /// </remarks>
     template<typename Enumerator>
     auto static Sum(Enumerator&& enumerator)
     {
@@ -35,12 +38,16 @@ public:
     /// <summary>
     /// Averages all elements in the enumerator.
     /// </summary>
+    /// <remarks>
+    /// If the enumerator is empty, the result is the default value of the element type.
+    /// Check for emptiness before calling this method if the default value is not the desired result.
+    /// </remarks>
     template<typename Enumerator>
     auto static Average(Enumerator&& enumerator)
     {
         using Aggregate = std::remove_reference_t<decltype(*enumerator)>;
 
-        Aggregate sum {};
+        Aggregate sum{};
         int32 count = 0;
         for (; enumerator; ++enumerator)
         {
@@ -49,5 +56,63 @@ public:
         }
 
         return sum / static_cast<Aggregate>(count);
+    }
+
+    /// <summary>
+    /// Averages all elements in the enumerator.
+    /// </summary>
+    /// <remarks> Calling this method on an empty enumerator is UB. </remarks>
+    template<typename Enumerator>
+    auto static Min(Enumerator&& enumerator)
+    {
+        using Aggregate = std::remove_reference_t<decltype(*enumerator)>;
+
+        ASSERT((enumerator)); // Enumerator must not be empty.
+
+        Aggregate min = *enumerator;
+        for (; enumerator; ++enumerator)
+            min = Math::Min(min, *enumerator);
+
+        return min;
+    }
+
+    /// <summary>
+    /// Averages all elements in the enumerator.
+    /// </summary>
+    /// <remarks> Calling this method on an empty enumerator is UB. </remarks>
+    template<typename Enumerator>
+    auto static Max(Enumerator&& enumerator)
+    {
+        using Aggregate = std::remove_reference_t<decltype(*enumerator)>;
+
+        ASSERT((enumerator)); // Enumerator must not be empty.
+
+        Aggregate max = *enumerator;
+        for (; enumerator; ++enumerator)
+            max = Math::Max(max, *enumerator);
+
+        return max;
+    }
+
+
+    /// <summary>
+    /// Finds the extremum element in the enumerator.
+    /// Predicate returns true if the first argument is better than the second.
+    /// </summary>
+    /// <remarks> Calling this method on an empty enumerator is UB. </remarks>
+    template<typename Enumerator, typename Predicate>
+    auto static Extremum(Enumerator&& enumerator, Predicate&& predicate) -> decltype(*enumerator)
+    {
+        ASSERT((enumerator)); // Enumerator must not be empty.
+
+        auto& extremum = *enumerator;
+        for (; enumerator; ++enumerator)
+
+        if (predicate(*enumerator, extremum))
+        {
+            extremum = *enumerator;
+        }
+
+        return extremum;
     }
 };
--- Tests/QueryingTests.cpp
+++ Tests/QueryingTests.cpp
@@ -4,33 +4,20 @@
 
 #include "Collections/Array.h"
 #include "Algorithms/Querying.h"
+#include "Allocators/FixedAlloc.h"
 
-TEST(Querying, Count)
+TEST(QueryingTests, Count)
 {
-    Array<int32> array;
+    using namespace Querying;
+
+    Array<int32, FixedAlloc<4 * sizeof(int32)>> array;
     array.Add(1);
     array.Add(2);
     array.Add(3);
-    auto count = Querying::Count(array.Enumerate());
-    ASSERT_EQ(count, 3);
-}
 
-TEST(Querying, Sum)
-{
-    Array<int32> array;
-    array.Add(1);
-    array.Add(2);
-    array.Add(3);
-    auto sum = Querying::Sum(array.Enumerate());
-    ASSERT_EQ(sum, 6);
-}
-
-TEST(Querying, Average)
-{
-    Array<int32> array;
-    array.Add(1);
-    array.Add(2);
-    array.Add(3);
-    auto average = Querying::Average(array.Enumerate());
-    ASSERT_EQ(average, 2);
+    ASSERT_EQ(Count    (array.Enumerate()), 3);
+    ASSERT_EQ(Sum      (array.Enumerate()), 6);
+    ASSERT_EQ(Average  (array.Enumerate()), 2);
+    ASSERT_EQ(Min      (array.Enumerate()), 1);
+    ASSERT_EQ(Max      (array.Enumerate()), 3);
 }
