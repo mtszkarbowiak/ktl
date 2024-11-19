815b0e0d8eadf1fb39734f3669570b5a6de6e25a
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2024-11-19 04:11:57 +0100

    Test macros info

--- Core/Debugging/LifecycleTracker.h
+++ Core/Debugging/LifecycleTracker.h
@@ -92,7 +92,24 @@ public:
 };
 
 
-// Very hacky macros ahead!
+// /!\ Very hacky macros ahead! /!\
+//
+// Their underlying objective is to create a lifecycle tracking scope for a given test case.
+// This is difficult, because each tracker requires a shared state between other trackers.
+// They fight with each other for the access to that shared state. One of the solutions would
+// be to dispatch the tests synchronously, but that would be a huge performance hit.
+// Instead, completely different approach is taken:
+//
+// In Google's 'gtest' each test is its own class. This means that each class creates a scope
+// we can assign shared state to. This is exactly what the macros do - they create a class
+// per test case and use tag dispatching to assign the shared state to the tracker class.
+// This way, each test case has its own shared state and the trackers don't fight with each other.
+// Unfortunately, this approach uses one very strong assumption: That the test cases are not run
+// in parallel. This is a very strong assumption, but it is the only way to make this work.
+//
+// Note: Using a reference to the shared state would be a better solution, but it would require:
+// a) external linkage b) would suffer the same lack of thread safety.
+
 
 #define LIFECYCLE_TEST_INTO \
     using ThisType = std::remove_reference_t<decltype(*this)>; \
