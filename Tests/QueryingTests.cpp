9146b7ca234f58e90d0517de704f37fa4f31ff38
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2024-11-22 18:25:13 +0100

    Select query test

--- Tests/QueryingTests.cpp
+++ Tests/QueryingTests.cpp
@@ -6,28 +6,52 @@
 #include "Allocators/FixedAlloc.h"
 #include "Collections/Array.h"
 #include "Collections/Ring.h"
+#include "Math/Stastics.h"
 
 
-TEST(QueryingTests, ArrayHeap_Count)
+TEST(QueryingTests_Count, ArrayHeap)
 {
     const auto array = Array<int32, HeapAlloc>::Of({ 1, 2, 3 });
-    ASSERT_EQ(Querying::Count(array.Vals()), 3);
+    GTEST_ASSERT_EQ(Querying::Count(array.Vals()), 3);
 }
 
-TEST(QueryingTests, ArrayFixed_Count)
+TEST(QueryingTests_Count, ArrayFixed)
 {
     const auto array = Array<int32, FixedAlloc<4 * sizeof(int32)>>::Of({ 1, 2, 3 });
-    ASSERT_EQ(Querying::Count(array.Vals()), 3);
+    GTEST_ASSERT_EQ(Querying::Count(array.Vals()), 3);
 }
 
-TEST(QueryingTests, RingHeap_Count)
+TEST(QueryingTests_Count, RingHeap)
 {
     const auto ring = Ring<int32, HeapAlloc>::Of({ 1, 2, 3 });
-    ASSERT_EQ(Querying::Count(ring.Vals()), 3);
+    GTEST_ASSERT_EQ(Querying::Count(ring.Vals()), 3);
 }
 
-TEST(QueryingTests, RingFixed_Count)
+TEST(QueryingTests_Count, RingFixed)
 {
     const auto ring = Ring<int32, FixedAlloc<4 * sizeof(int32)>>::Of({ 1, 2, 3 });
-    ASSERT_EQ(Querying::Count(ring.Vals()), 3);
+    GTEST_ASSERT_EQ(Querying::Count(ring.Vals()), 3);
+}
+
+
+TEST(QueryingTests_Select, Array)
+{
+    using namespace Querying;
+    using namespace Statistics;
+
+    const auto array = Array<int32, HeapAlloc>::Of({ 1, 2, 3 });
+    const auto selector = [](const int32 value) { return value * 2; };
+
+    using SelectX2 = Select<decltype(selector)>;
+
+    auto selected = array.Vals()
+        | Select<decltype(selector)>(selector)
+        | SelectX2(selector);
+
+    const auto expected = Array<int32, HeapAlloc>::Of({ 4, 8, 12 });
+
+    GTEST_ASSERT_EQ(
+        Sum(MOVE(selected)), 
+        Sum(expected.Vals())
+    );
 }
