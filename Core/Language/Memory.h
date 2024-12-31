9d56667c0e298cec44c0d89d093ffbc66cfed3bd
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2024-12-31 02:39:51 +0100

    Memory macros update

--- Core/Language/Memory.h
+++ Core/Language/Memory.h
@@ -6,12 +6,21 @@
 #include <cstring>
 
 
+/// <summary>
+/// Type alias for a 1-byte unsigned integer, distinguished from a character.
+/// </summary>
+/// <remarks>
+/// This type is used for raw memory manipulation to avoiding interpreting
+/// the value as a type which could violate the Strict Aliasing Rule.
+/// https://en.cppreference.com/w/c/language/object
+/// </remarks>
 using byte = unsigned char;
 
 
-#define MALLOC(size)            malloc(size)
-#define REALLOC(ptr, size)      realloc((ptr), (size))
-#define FREE(ptr)               free(ptr)
+#define MALLOC(size)             malloc(size)
+#define REALLOC(ptr, size)       realloc((ptr), (size))
+#define FREE(ptr)                free(ptr)
 
-#define MEMCPY(dst, src, size)  memcpy((dst), (src), (size))
-#define MEMMOVE(dst, src, size) memmove((dst), (src), (size))
+#define MEMCPY(dst, src, size)   memcpy((dst), (src), (size))
+#define MEMSET(dst, value, size) memset((dst), (value), (size))
+#define MEMMOVE(dst, src, size)  memmove((dst), (src), (size))
