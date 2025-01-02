3914b7d562654b4f7834f9041f088898b705958c
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2025-01-02 20:33:51 +0100

    Assertion types default levels

--- Core/Debugging/Assertions.h
+++ Core/Debugging/Assertions.h
@@ -11,6 +11,15 @@
 
 // WARNING: Assertions have been categorized ONLY to allow balance assertions power with performance.
 
+// --- Assertions Level ---
+// 0. No assertions.
+// 1. Basic assertions.
+// 2. Full assertions.
+// 3. Extended assertions.
+#ifndef ASSERTIONS_LEVEL
+    #define ASSERTIONS_LEVEL 3
+#endif
+
 
 // --- Allocators Assertions ---
 // Allocator are fundamental types, which can be invoked very frequently.
@@ -18,13 +27,13 @@
 // they may be disabled in production builds to improve performance, without losing much safety.
 
 #ifndef ALLOCATOR_ASSERTIONS_ENABLED
-    #define ALLOCATOR_ASSERTIONS_ENABLED 1
+    #define ALLOCATOR_ASSERTIONS_ENABLED (ASSERTIONS_LEVEL >= 2)
 #endif
 
 #ifdef ALLOCATOR_ASSERTIONS_ENABLED
     #define ASSERT_ALLOCATOR_SAFETY(x) ASSERT(x)
 #else
-    #define ASSERT_ALLOC(x)
+    #define ASSERT_ALLOCATOR_SAFETY(x)
 #endif
 
 
@@ -33,7 +42,7 @@
 // They are useful when developing those collections, but may be disabled in production builds.
 
 #ifndef COLLECTION_INTEGRITY_ASSERTIONS_ENABLED
-    #define COLLECTION_INTEGRITY_ASSERTIONS_ENABLED 1
+    #define COLLECTION_INTEGRITY_ASSERTIONS_ENABLED (ASSERTIONS_LEVEL >= 3)
 #endif
 
 #ifdef COLLECTION_INTEGRITY_ASSERTIONS_ENABLED
@@ -49,7 +58,7 @@
 // It is recommended to postpone disabling them after the codebase is declared stable.
 
 #ifndef COLLECTION_SAFE_MOD_ASSERTIONS_ENABLED
-    #define COLLECTION_SAFE_MOD_ASSERTIONS_ENABLED 1
+    #define COLLECTION_SAFE_MOD_ASSERTIONS_ENABLED (ASSERTIONS_LEVEL >= 2)
 #endif
 
 #ifdef COLLECTION_SAFE_MOD_ASSERTIONS_ENABLED
@@ -65,7 +74,7 @@
 // It is recommended to postpone disabling them after the codebase is declared stable.
 
 #ifndef COLLECTION_SAFE_ACCESS_ASSERTIONS_ENABLED
-    #define COLLECTION_SAFE_ACCESS_ASSERTIONS_ENABLED 1
+    #define COLLECTION_SAFE_ACCESS_ASSERTIONS_ENABLED (ASSERTIONS_LEVEL >= 2)
 #endif
 
 #ifdef COLLECTION_SAFE_ACCESS_ASSERTIONS_ENABLED
