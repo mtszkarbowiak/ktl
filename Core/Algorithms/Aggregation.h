0f66f92c048d592397938829063052e3d2372195
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2025-01-15 00:59:23 +0100

    Collecting queries *

---
+++ Core/Algorithms/Aggregation.h
@@ -0,0 +1,56 @@
+// GameDev Template Library - Created by Mateusz Karbowiak 2024-25
+// Repository: https://github.com/mtszkarbowiak/mk-stl/
+//
+// This project is licensed under the MIT License, which allows you to use, modify, distribute,
+// and sublicense the code as long as the original license is included in derivative works.
+// See the LICENSE file for more details.
+
+#pragma once
+
+#include "Algorithms/Querying.h
+#include "Collections/Array.h"
+
+namespace Querying
+{
+    /// <summary>
+    /// Collects the elements of the collection into an array,
+    /// with the specified capacity.
+    /// </summary>
+    template<
+        typename _C,
+        typename A,
+        int32(&G)(int32)
+    >
+    NO_DISCARD
+    auto ToArray(_C&& cursor, const int32 capacity) -> Array<decltype(*cursor), A, G>
+    {
+        using ElementType = decltype(*cursor);
+        Array<ElementType, A, G> collector{ capacity };
+
+        for (; cursor; ++cursor)
+            collector.Add(*cursor);
+
+        return collector.ToArray();
+    }
+
+    /// <summary>
+    /// Collects the elements of the collection into an array,
+    /// which capacity is predicted by the cursor hint.
+    /// </summary>
+    template<
+        typename _C,
+        typename A,
+        int32(&G)(int32)
+    >
+    NO_DISCARD
+    auto ToArray(_C&& cursor) -> Array<decltype(*cursor), A, G>
+    {
+        const int32 predictedCount = cursor.Hint().Max;
+        return ToArray<_C, A, G>(FORWARD(_C, cursor), predictedCount);
+    }
+}
+
+//TODO(mtszkarbowiak) Implement GroupBy with Dictionary<TKey, Array<TValue>>
+//TODO(mtszkarbowiak) Implement OrderBy with Array<T>
+//TODO(mtszkarbowiak) Implement Reverse with Array<T>
+//TODO(mtszkarbowiak) Implement Distinct with HashSet<T>
