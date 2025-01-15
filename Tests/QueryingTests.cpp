b0c1558d2d168f1406bc10ce574f1e984d783c14
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2025-01-15 01:18:22 +0100

    Querying tests fix and improvs

--- Tests/QueryingTests.cpp
+++ Tests/QueryingTests.cpp
@@ -7,6 +7,7 @@
 
 #include <gtest/gtest.h>
 
+#include "Algorithms/Aggregation.h"
 #include "Algorithms/Querying.h"
 #include "Allocators/FixedAlloc.h"
 #include "Collections/Array.h"
@@ -15,32 +16,32 @@
 #include "Math/Stastics.h"
 
 
-TEST(QueryingTests_Count, ArrayHeap)
+TEST(QueryCount, ArrayHeap)
 {
     const auto array = Array<int32, HeapAlloc>::Of({ 1, 2, 3 });
     GTEST_ASSERT_EQ(Querying::Count(array.Values()), 3);
 }
 
-TEST(QueryingTests_Count, ArrayFixed)
+TEST(QueryCount, ArrayFixed)
 {
     const auto array = Array<int32, FixedAlloc<4 * sizeof(int32)>>::Of({ 1, 2, 3 });
     GTEST_ASSERT_EQ(Querying::Count(array.Values()), 3);
 }
 
-TEST(QueryingTests_Count, RingHeap)
+TEST(QueryCount, RingHeap)
 {
     const auto ring = Ring<int32, HeapAlloc>::Of({ 1, 2, 3 });
     GTEST_ASSERT_EQ(Querying::Count(ring.Values()), 3);
 }
 
-TEST(QueryingTests_Count, RingFixed)
+TEST(QueryCount, RingFixed)
 {
     const auto ring = Ring<int32, FixedAlloc<4 * sizeof(int32)>>::Of({ 1, 2, 3 });
     GTEST_ASSERT_EQ(Querying::Count(ring.Values()), 3);
 }
 
 
-TEST(QueryingTests_Select, Array)
+TEST(QuerySelect, Array)
 {
     using namespace Querying;
     using namespace Statistics;
@@ -62,7 +63,7 @@ TEST(QueryingTests_Select, Array)
     );
 }
 
-TEST(QueryingTests_Where, Array)
+TEST(QueryWhere, Array)
 {
     using namespace Querying;
     using namespace Statistics;
@@ -83,7 +84,7 @@ TEST(QueryingTests_Where, Array)
     );
 }
 
-TEST(QueryingTests_SelectWhere, Array)
+TEST(QuerySelectWhere, Array)
 {
     using namespace Querying;
     using namespace Statistics;
@@ -114,39 +115,25 @@ TEST(QueryingTests_SelectWhere, Array)
     }
 }
 
-//TEST(QueryingTest_Sum, Dictionary)
-//{
-//    using namespace Querying;
-//    using namespace Statistics;
-//
-//    Dictionary<int32, float> dict;
-//    dict.Add(1, 5.0f);
-//    dict.Add(2, 8.0f);
-//    dict.Add(3, 17.0f);
-//    dict.Add(4, 3.0f);
-//    dict.Add(5, 9.0f);
-//    dict.Add(6, 11.0f);
-//    dict.Add(7, 2.0f);
-//
-//    // Calculate the sum of all even keys
-//
-//    auto where = [](const int32 key) { return key % 2 == 0; };
-//
-//    const int32 sum = dict.Keys()
-//        | Where<decltype(where)>(where)
-//        | ToSum();
-//
-//    GTEST_ASSERT_EQ(sum, 12);
-//
-//    // Calculate the average of squares of values above 5.
-//
-//    auto where2 = [](const float value) { return value > 5.0f; };
-//    auto select = [](const float value) { return value * value; };
-//
-//    const float wtfNumba = dict.Values()
-//        | Where<decltype(where2)>(where2)
-//        | Select<decltype(select)>(select)
-//        | ToAverage();
-//
-//    GTEST_ASSERT_GE(wtfNumba, 138.0f);
-//}
+
+
+TEST(QueryToArray, Array)
+{
+    using namespace Querying;
+    using namespace Statistics;
+    const auto array = Array<int32>::Of({ 1, 2, 3, 4, 5 });
+    const auto selector = [](const int32 value) { return value * 2; };
+    const auto predicate = [](const int32 value) { return value % 4 == 0; };
+
+    const auto result = ToArray(
+        array.Values()
+            | Select<decltype(selector)>(selector)
+            | Where<decltype(predicate)>(predicate)
+    );
+
+    const auto expected = Array<int32>::Of({ 4, 8 });
+    GTEST_ASSERT_EQ(
+        Sum(result.Values()),
+        Sum(expected.Values())
+    );
+}
