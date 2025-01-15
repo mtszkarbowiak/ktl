fe791ff4df8d5b3609f3ec03fcd1dff9315cb319
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2025-01-15 02:43:11 +0100

    Repo link update

--- Core/Algorithms/Aggregation.h
+++ Core/Algorithms/Aggregation.h
@@ -1,5 +1,5 @@
 // GameDev Template Library - Created by Mateusz Karbowiak 2024-25
-// Repository: https://github.com/mtszkarbowiak/mk-stl/
+// Repository: https://github.com/mtszkarbowiak/ktl/
 //
 // This project is licensed under the MIT License, which allows you to use, modify, distribute,
 // and sublicense the code as long as the original license is included in derivative works.
