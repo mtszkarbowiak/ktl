216ea031b10a04beccfebe493444f66c83d7fce1
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2025-01-22 16:52:16 +0100

    Scheduler example test

--- Tests/JobTests.cpp
+++ Tests/JobTests.cpp
@@ -12,11 +12,32 @@
 TEST(JobScheduler, Simple)
 {
     JobScheduler scheduler{ 4 };
+
+    const auto now = std::chrono::high_resolution_clock::now();
+
     scheduler.Dispatch([]()
     {
-        std::cout << "Job 1 - Start" << std::endl;
+        std::cout << "Job 1 - Start\n";
         std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "Job 1 - End" << std::endl;
+        std::cout << "Job 1 - End\n";
     });
+    scheduler.Dispatch([]()
+    {
+        std::cout << "Job 2 - Start\n";
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "Job 2 - End\n";
+    });
+    scheduler.Dispatch([]()
+    {
+        std::cout << "Job 3 - Start\n";
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "Job 3 - End\n";
+    });
+
+    const auto then = std::chrono::high_resolution_clock::now();
+    const auto duration = std::chrono::duration_cast<std::chrono::seconds>(then - now);
+    const float durationSeconds = duration.count();
+    GTEST_ASSERT_LT(durationSeconds, 2);
+
     scheduler.WaitAll();
 }
