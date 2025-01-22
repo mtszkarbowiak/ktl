9cfef37294ed20d575c030c34c3bf44389e8a043
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2025-01-22 17:21:55 +0100

    Scheduler test parametrization

--- Tests/JobTests.cpp
+++ Tests/JobTests.cpp
@@ -9,66 +9,51 @@
 
 #include "Threading/JobScheduler.h"
 
+template<int Index, int DurationMs = 1000>
+struct TestJob
+{
+    void operator()()
+    {
+        const std::string startInfo = "Job " + std::to_string(Index) + " - Start\n";
+        const std::string endInfo = "Job " + std::to_string(Index) + " - End\n";
+
+        std::cout << startInfo;
+        std::this_thread::sleep_for(std::chrono::milliseconds(DurationMs));
+        std::cout << endInfo;
+    }
+};
+
 TEST(JobScheduler, WaitAllScheduled)
 {
     JobScheduler scheduler{ 4 };
 
     const auto now = std::chrono::high_resolution_clock::now();
 
-    scheduler.Dispatch([]()
-    {
-        std::cout << "Job 1 - Start\n";
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "Job 1 - End\n";
-    });
-    scheduler.Dispatch([]()
-    {
-        std::cout << "Job 2 - Start\n";
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "Job 2 - End\n";
-    });
-    scheduler.Dispatch([]()
-    {
-        std::cout << "Job 3 - Start\n";
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "Job 3 - End\n";
-    });
-
-    const auto then = std::chrono::high_resolution_clock::now();
-    const auto duration = std::chrono::duration_cast<std::chrono::seconds>(then - now);
-    const float durationSeconds = duration.count();
-    GTEST_ASSERT_LT(durationSeconds, 2);
+    scheduler.Dispatch(TestJob<1>{});
+    scheduler.Dispatch(TestJob<2>{});
+    scheduler.Dispatch(TestJob<3>{});
 
     scheduler.WaitAll();
+
+    const auto then = std::chrono::high_resolution_clock::now();
+    const auto durationMs = std::chrono::duration_cast<std::chrono::milliseconds>(then - now).count();
+    GTEST_ASSERT_LT(durationMs, 1100);
 }
 
 TEST(JobScheduler, WaitScheduled)
 {
     JobScheduler scheduler{ 4 };
     const auto now = std::chrono::high_resolution_clock::now();
-    const auto label1 = scheduler.Dispatch([]()
-    {
-        std::cout << "Job 1 - Start\n";
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "Job 1 - End\n";
-    });
-    const auto label2 = scheduler.Dispatch([]()
-    {
-        std::cout << "Job 2 - Start\n";
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "Job 2 - End\n";
-    });
-    const auto label3 = scheduler.Dispatch([]()
-    {
-        std::cout << "Job 3 - Start\n";
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        std::cout << "Job 3 - End\n";
-    });
-    const auto then = std::chrono::high_resolution_clock::now();
-    const auto duration = std::chrono::duration_cast<std::chrono::seconds>(then - now);
-    const float durationSeconds = duration.count();
-    GTEST_ASSERT_LT(durationSeconds, 2);
+
+    const JobScheduler::JobLabel label1 = scheduler.Dispatch(TestJob<1>{});
+    const JobScheduler::JobLabel label2 = scheduler.Dispatch(TestJob<2>{});
+    const JobScheduler::JobLabel label3 = scheduler.Dispatch(TestJob<3>{});
+
     scheduler.Wait(label1);
     scheduler.Wait(label2);
     scheduler.Wait(label3);
+
+    const auto then = std::chrono::high_resolution_clock::now();
+    const auto durationMs = std::chrono::duration_cast<std::chrono::milliseconds>(then - now).count();
+    GTEST_ASSERT_LT(durationMs, 1100);
 }
