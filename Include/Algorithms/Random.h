5daf567eb0b9a24ad9ee711b6b120a3b76bb4e66
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2025-02-03 17:06:02 +0100

    `XorwowRng`

---
+++ Include/Algorithms/Random.h
@@ -0,0 +1,59 @@
+// GameDev Template Library - Created by Mateusz Karbowiak 2024-25
+// Repository: https://github.com/mtszkarbowiak/ktl/
+//
+// This project is licensed under the MIT License, which allows you to use, modify, distribute,
+// and sublicense the code as long as the original license is included in derivative works.
+// See the LICENSE file for more details.
+
+#pragma once
+
+#include "Language/Keywords.h"
+#include "Language/Templates.h"
+#include "Types/Nullable.h"
+#include "Types/Numbers.h"
+#include "Types/SizeHint.h"
+
+/// <summary>
+/// Psuedo-random number generator based on the Xorwow algorithm.
+/// It can be used as a pull iterator to generate random numbers.
+/// </summary>
+class XorwowRng final
+{
+PRIVATE:
+    uint32 _state[5];
+    uint32 _d;
+
+
+public:
+    explicit
+    XorwowRng(uint32 seed = 0);
+
+    NO_DISCARD FORCE_INLINE
+    operator bool() const
+    {
+        return true; // Always valid.
+    }
+
+    NO_DISCARD FORCE_INLINE
+    auto operator*() const -> uint32
+    {
+        return _state[4] + _d;
+    }
+
+    MAY_DISCARD
+    auto operator++() -> XorwowRng&;
+
+    MAY_DISCARD
+    auto operator++(int) -> XorwowRng
+    {
+        auto copy = *this;
+        ++*this;
+        return copy;
+    }
+
+    NO_DISCARD
+    auto Hint() const -> SizeHint
+    {
+        return SizeHint{ 0, Nullable<::Index>{} };
+    }
+};
