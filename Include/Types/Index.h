0113f6fd7009788eee58b79169eb11bfb0ed68a9
Mateusz Karbowiak <69864511+mtszkarbowiak@users.noreply.github.com>
2025-03-01 15:01:51 +0100

    `Index` from `Nullable`

--- Include/Types/Index.h
+++ Include/Types/Index.h
@@ -8,10 +8,10 @@
 #pragma once
 
 #include "Debugging/Assertions.h"
+#include "Language/Communism.h"
 #include "Language/Keywords.h"
-#include "Language/Templates.h"
-#include "Types/Nullable.h"
 #include "Types/Numbers.h"
+#include "Types/Base.h"
 
 /// <summary>
 /// Wrapper over an integer value that represents an index in a collection,
